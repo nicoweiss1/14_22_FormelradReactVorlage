435f3788dc0332ce575dd20fb59c448da8df1d62
nicoweiss1 <148877618+nicoweiss1@users.noreply.github.com>
2025-06-05 11:09:13 +0200

    0018-Calculate-r-and-p

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -31,6 +31,10 @@ export default function Formelrad() {
             setValues(values => ({ ...values, i: values.u / values.r }));
             setValues(values => ({ ...values, p: values.u * values.u / values.r }));
             setColors(colors => ({ ...colors, i: "red", p: "red" }));
+        } else {
+            /*calculate r and p */
+            setValues(values => ({ ...values, r: values.u / values.i }));
+            setValues(values => ({ ...values, p: values.u * values.i }));
         }
     }
 
